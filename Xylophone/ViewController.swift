fedb6a69a9a3e0ad17e937156e7497e5c882d771
Anthony Petrov <beavean@gmail.com>
2022-06-30 16:55:16 +0300

    Animation added

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var player: AVAudioPlayer!
+    
+    
+    @IBOutlet var keyFrames: [UIButton]!
+    
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let url = Bundle.main.url(forResource: sender.currentTitle, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        for item in keyFrames {
+            if item.currentTitle == sender.currentTitle {
+                UIView.animate(withDuration: 0.1, delay: 0, options: []) {
+                    item.alpha = 0.7
+                    UIView.animate(withDuration: 0.1, delay: 0, options: []) {
+                        item.alpha = 1
+                    }
+                }
+            }
+        }
+        player.play()
     }
-
-
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        for item in keyFrames {
+            item.layer.cornerRadius = item.frame.height / 2
+        }
+    }
 }
+
+
+
 
